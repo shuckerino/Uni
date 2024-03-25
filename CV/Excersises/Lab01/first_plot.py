75caa160e239157361057baf8f50deba5b17e3e5
Rino-Laptop <rino.schu@gmail.com>
2024-03-25 20:16:48 +0100

    Plot for CV Exc01

---
+++ CV/Excersises/Lab01/first_plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create the two vectors which should be plotted
+data = np.array([[1,6],
+                  [9,22]])
+# Get data points for time and velocity
+time = data[:, 0]
+speed = data[:, 1]
+
+fig = plt.figure(figsize = (20,20))
+plt.plot(time,speed, "r-", label="Function of car velocity over time")
+
+# Label the plot
+plt.xlabel("Time in sec")
+plt.ylabel("Velocity in km/h")
+plt.title("Data points of car velocity")
+plt.grid(True)
+plt.legend()
+
+# Show the plot
+plt.show()
---
+++ CV/Excersises/Lab01/solutions.md
@@ -0,0 +1,10 @@
+
+1b) 
+- allgemeine Gleichung: y = a*x + b
+
+I) 6 = a*1 + b -> b = 6-a
+II) 22 = a*9 + b -> 16 = 8a -> a = 2, b = 4
+
+
+1c) 
+
