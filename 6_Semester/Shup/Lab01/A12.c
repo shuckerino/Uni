0edf3ace873d06e8c2640476c8f0be5a059913a1
Rino-Laptop <rino.schu@gmail.com>
2024-03-27 15:10:22 +0100

    Fixed A12

--- 6_Semester/Shup/Lab01/A12.c
+++ 6_Semester/Shup/Lab01/A12.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 int main(int argc, char *argv[]){
     int num_args = atoi(argv[1]);
-    char** all_names[argc] = (char**)malloc(num_args * sizeof(char*));
+    char** all_names = (char**)malloc(num_args * sizeof(char*));
 
     if (argc != 2){
 	    printf("Invalid CLI arguments...Please only specify one argument with the number of parameters that should be read in!\n");
-    return -1;
+        return -1;
     }
 
     for (int i=0; i < num_args; i++){
@@ -17,7 +17,10 @@ int main(int argc, char *argv[]){
 	    fflush(stdout);
 
 	    // read input
-	    fgets(buffer, sizeof(buffer), stdin);
+	    if (fgets(buffer, sizeof(buffer), stdin) == NULL){
+            printf("Error reading input!!");
+            return -1;
+        };
             
 	    // allocate memory and copy buffer into array of pointers
 	    all_names[i] = (char*)malloc(strlen(buffer) + 1);
@@ -25,7 +28,7 @@ int main(int argc, char *argv[]){
     }
     
     // output in reverse order
-    int count = 1;
+    int count = 2;
     for (int i = num_args-1; i >= 0; i--){
 	printf("%d. name: %s\n", count, all_names[i]);
         count++; 
