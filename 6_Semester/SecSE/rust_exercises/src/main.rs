7aa608615aaea7a42573cfef896608f89708a57a
Rino-Laptop <rino.schu@gmail.com>
2024-03-29 16:54:52 +0100

    Blank

--- 6_Semester/SecSE/rust_exercises/src/main.rs
+++ 6_Semester/SecSE/rust_exercises/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+
+    let x : i32 = 5;
+
+    if x == 5 {
+       print!("X is five!");
+
+
+    }
+    else {
+        print!("X is not five!!!!!");
+    }
 }
---
+++ 6_Semester/SecSE/rust_exercises/target/debug/deps/rust_exercises-26b917781c2ad2db.d
@@ -0,0 +1,5 @@
+/home/rino/Desktop/Uni/6_Semester/SecSE/rust_exercises/target/debug/deps/librust_exercises-26b917781c2ad2db.rmeta: src/main.rs
+
+/home/rino/Desktop/Uni/6_Semester/SecSE/rust_exercises/target/debug/deps/rust_exercises-26b917781c2ad2db.d: src/main.rs
+
+src/main.rs:
