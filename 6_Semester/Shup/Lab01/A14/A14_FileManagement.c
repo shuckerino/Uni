db483fcd5db9c928130ccf4634d97fd3318623db
Rino-Laptop <rino.schu@gmail.com>
2024-03-29 22:29:05 +0100

    Writing file for Shup/A14

--- 6_Semester/Shup/Lab01/A14/A14_FileManagement.c
+++ 6_Semester/Shup/Lab01/A14/A14_FileManagement.c
@@ -6,46 +6,78 @@ umgekehrter Reihenfolge in eine andere ASCII-Datei mit Namen
 #include <stdlib.h>
 #include <string.h>
 
+#define INIT_CAPACITY 10
+
 int main(int argc, char **argv) {
 
-  char *filename = argv[1];
+  char *input_filename = argv[1];
+  char *output_filename = argv[2];
 
-  if (!filename) {
-    printf("You need to specify the filename that should be read as the first "
-           "command line argument!\n");
+  if (!input_filename || !output_filename) {
+    printf("You need to specify the input filename as the first and the output "
+           "filename as the second argument!\n");
     return -1;
   }
 
-  FILE *file = fopen(filename, "r");
+  FILE *input_file = fopen(input_filename, "r");
+  FILE *output_file = fopen(output_filename, "w");
 
-  if (!file) {
-    printf("Could not read file %s\n", filename);
+  if (!input_file) {
+    printf("Could not read input filename %s\n", input_filename);
     return -1;
   }
 
-  // printf("After opening file");
-  //  do stuff
-  char **names = (char **)malloc(10 * sizeof(char));
+  if (!output_file) {
+    printf("Could not read output filename %s\n", output_filename);
+    return -1;
+  }
+
+  // read and write data
+  char **names = (char **)malloc(INIT_CAPACITY * sizeof(char));
   char line[128];
   int line_count = 0;
-  // printf("Before while loop");
+  int current_capacity = INIT_CAPACITY;
 
-  while (fgets(line, sizeof(line), file) != NULL) {
+  while (fgets(line, sizeof(line), input_file) != NULL) {
+
+    if (line_count >= current_capacity) {
+      current_capacity *= 2;
+      char **temp = (char **)realloc(names, current_capacity * sizeof(char *)); // throws error invalid next size
+
+      // check if realloc failed
+      if (!temp) {
+        printf("Memoroy reallocation failed, aborting...");
+        for (int i = 0; i < line_count; i++) {
+          free(names[i]);
+        }
+        free(names);
+        return -1;
+      }
+      names = temp;
+    }
+
     names[line_count] = malloc(strlen(line) + 1);
     strcpy(names[line_count], line);
-    //names[line_count] = line;
+    // names[line_count] = line;
     line_count++;
   }
 
-  //fclose(file); // throws error, even when calling at end of program
-    
+  // print to stdout
   int count = 1;
   for (int i = line_count - 1; i >= 0; i--) {
-    printf("%d. Name is: %s", count, names[i]); // no new line char because it was already contained in the file
+    printf("%d. Name is: %s", count,
+           names[i]); // no new line char because it was already contained in
+                      // the input_file
     count++;
-    free(names[i]);
   }
 
+  // write to output file
+  for (int i = line_count - 1; i >= 0; i--) {
+    fprintf(output_file, "%s", names[i]);
+    free(names[i]);
+  }
+  // fclose(input_file); // throws "double free or corruption" error
+  fclose(output_file);
   free(names);
 
   return 0;
