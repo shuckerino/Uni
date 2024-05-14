ada1512f0fb2ccab98e1d0d8b16b6d6a8b48fb67
Schuster, Rinaldo <rino.schu@gmail.com>
2024-05-14 14:46:34 +0200

    Right synchronisation in a33

--- Shup/shup2024-Schuster-Rinaldo/blatt3/a33/a33.c
+++ Shup/shup2024-Schuster-Rinaldo/blatt3/a33/a33.c
@@ -18,22 +18,20 @@ struct shared_data
     int counselor_count;
 };
 
-// semaphores
-int mutex_sem, waiting_sem, counselor_sem, waiting_time_sem;
+// Semaphore IDs
+int mutex_sem, waiting_sem, counselor_sem;
 
-// shared memory
+// Shared memory ID
 int shared_memory_id;
 
-struct shared_data *shared_data;
-
+// Initialize semaphores and shared memory
 void init_semaphores_and_shared_memory()
 {
-    mutex_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0777);
-    waiting_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0777);
-    counselor_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0777);
-    waiting_time_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0777);
+    mutex_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0777);
+    waiting_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0777);
+    counselor_sem = semget(IPC_PRIVATE, 1, IPC_CREAT | 0777);
 
-    if (mutex_sem < 0 || waiting_sem < 0 || counselor_sem < 0 || waiting_time_sem < 0)
+    if (mutex_sem < 0 || waiting_sem < 0 || counselor_sem < 0)
     {
         printf("Error while creating semaphores!\n");
         exit(1);
@@ -41,17 +39,33 @@ void init_semaphores_and_shared_memory()
 
     // Initialize semaphores
     semctl(mutex_sem, 0, SETVAL, 1);
-    semctl(waiting_sem, 0, SETVAL, MAX_QUEUE_SIZE);
+    semctl(waiting_sem, 0, SETVAL, 0);
     semctl(counselor_sem, 0, SETVAL, MAX_COUNSELORS);
-    semctl(waiting_time_sem, 0, SETVAL, 1);
 
     // Initialize shared memory
-    // shared_memory_id = shmget(IPC_PRIVATE, sizeof(struct shared_data), IPC_CREAT | IPC_EXCL | 0777);
+    shared_memory_id = shmget(IPC_PRIVATE, sizeof(struct shared_data), IPC_CREAT | 0777);
 
-    // if (shared_memory_id < 0)
-    // {
-    //     printf("Error while creating shared memory!\n");
-    // }
+    if (shared_memory_id < 0)
+    {
+        printf("Error while creating shared memory!\n");
+        exit(1);
+    }
+
+    struct shared_data *shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
+    if (shared_data == (void *)-1)
+    {
+        printf("Error while attaching shared memory!\n");
+        exit(1);
+    }
+
+    shared_data->waiting_count = 0;
+    shared_data->counselor_count = MAX_COUNSELORS;
+
+    if (shmdt(shared_data) == -1)
+    {
+        printf("Error while detaching shared memory!\n");
+        exit(1);
+    }
 }
 
 int wait_sem(int semid)
@@ -79,124 +93,81 @@ int get_random_time(int min, int max)
 
 void handle_incoming_calls()
 {
-    // shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
-
-    // Check for failure
-    // if (shared_data == NULL)
-    // {
-    //     printf("Error while attaching shared memory address for incoming call handler!\n");
-    //     exit(1);
-    // }
-
-    // while (1)
-    // {
-    //     sleep(get_random_time(0, 2));
-
-    //     wait_sem(mutex_sem);
-    //     if (shared_data->waiting_count < MAX_QUEUE_SIZE)
-    //     {
-    //         shared_data->waiting_count++;
-    //         signal_sem(mutex_sem);
-
-    //         signal_sem(waiting_sem); // Signal that a call is waiting
-    //         printf("Call is added to the queue. Waiting count: %d\n", shared_data->waiting_count);
-    //     }
-    //     else
-    //     {
-    //         signal_sem(mutex_sem);
-    //         printf("The hotline is overloaded. Please try again later.\n");
-    //     }
-    // }
+    struct shared_data *shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
+    if (shared_data == (void *)-1)
+    {
+        printf("Error while attaching shared memory for incoming call handler!\n");
+        exit(1);
+    }
 
     while (1)
     {
         sleep(get_random_time(0, 2));
 
-        // wait_sem(mutex_sem);
-        int value = semctl(waiting_sem, 0, GETVAL, 0);
-        if (value > 0)
+        wait_sem(mutex_sem);
+        if (shared_data->waiting_count < MAX_QUEUE_SIZE)
         {
-            // signal_sem(mutex_sem);
-            wait_sem(waiting_sem); // Decrement the number of available spots in the waiting queue
+            shared_data->waiting_count++;
+            signal_sem(mutex_sem);
 
-            printf("Call is added to the queue.\n");
-
-            // Wait for an available counselor
-            wait_sem(counselor_sem);
-            printf("Call is being handled by a counselor.\n");
+            signal_sem(waiting_sem); // Increment to signal, that one more is waiting
+            printf("Call is added to the queue. Waiting count: %d\n", shared_data->waiting_count);
         }
         else
         {
-            // signal_sem(mutex_sem);
+            signal_sem(mutex_sem);
             printf("The hotline is overloaded. Please try again later.\n");
         }
     }
 
-    // detach address space from current process
-    // if (shmdt(shared_data) == -1)
-    // {
-    //     printf("Error while detaching shared memory from address space of current process!\n");
-    //     exit(1);
-    // }
+    if (shmdt(shared_data) == -1)
+    {
+        printf("Error while detaching shared memory for incoming call handler!\n");
+        exit(1);
+    }
 }
 
 void counselor()
 {
-    // shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
-
-    // // Check for failure
-    // if (shared_data == NULL)
-    // {
-    //     printf("Error while attaching shared memory address for counselor function!\n");
-    //     exit(1);
-    // }
-
-    // while (1)
-    // {
-    //     wait_sem(waiting_sem); // Wait for a call to be available
-    //     wait_sem(mutex_sem);
-    //     shared_data->counselor_count--;
-    //     shared_data->waiting_count--;
-    //     signal_sem(mutex_sem);
-
-    //     printf("Counselor takes next call.\n");
-    //     sleep(get_random_time(0, 5)); // simulate random duration of call between 0 and 5
-    //     printf("Counselor finishes current call.\n");
-
-    //     wait_sem(mutex_sem);
-    //     shared_data->counselor_count++;
-    //     shared_data->waiting_count++;
-    //     signal_sem(mutex_sem);
-    //     signal_sem(counselor_sem);
-    //     wait_sem(waiting_sem);
-    // }
-    while (1)
+    struct shared_data *shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
+    if (shared_data == (void *)-1)
     {
-        // Wait for an available call
-        int value = semctl(waiting_sem, 0, GETVAL, 0);
-        if (value < MAX_QUEUE_SIZE && value > 0)
-        {
-            signal_sem(waiting_sem);
-            printf("Counselor takes next call.\n");
-            sleep(get_random_time(0, 5)); // Simulate random duration of call
-            printf("Counselor finished call.\n");
-
-            // Signal that the counselor is available again
-            signal_sem(counselor_sem);
-        }
-        else
-        {
-            printf("No calls in queue.\n");
-            sleep(1);
-        }
+        printf("Error while attaching shared memory for counselor!\n");
+        exit(1);
     }
 
-    // detach address space from current process
-    // if (shmdt(shared_data) == -1)
-    // {
-    //     printf("Error while detaching shared memory from address space of current process!\n");
-    //     exit(1);
-    // }
+    int current_customer, current_counselor;
+
+    while (1)
+    {
+        wait_sem(waiting_sem); // wait for a call in queue
+
+        // decrease waiting counter
+        wait_sem(counselor_sem);
+        wait_sem(mutex_sem);
+        current_customer = shared_data->waiting_count;
+        current_counselor = shared_data->counselor_count;
+        printf("Counselor %d takes call from customer Nr. %d.\n", current_counselor, current_customer);
+        shared_data->counselor_count--;
+        shared_data->waiting_count--;
+        signal_sem(mutex_sem);
+
+        sleep(get_random_time(0, 5)); // Simulate random duration of call between 0 and 5
+        
+        printf("Counselor %d finishes current call with customer %d.\n", current_counselor, current_customer);
+
+        signal_sem(counselor_sem); // Signal that the counselor is available again
+
+        wait_sem(mutex_sem);
+        shared_data->counselor_count++;
+        signal_sem(mutex_sem);
+    }
+
+    if (shmdt(shared_data) == -1)
+    {
+        printf("Error while detaching shared memory for counselor!\n");
+        exit(1);
+    }
 }
 
 int main()
@@ -205,57 +176,45 @@ int main()
 
     init_semaphores_and_shared_memory();
 
-    // shared_data = (struct shared_data *)shmat(shared_memory_id, NULL, 0);
-
-    // shared_data->waiting_count = 0;
-    // shared_data->counselor_count = MAX_COUNSELORS;
-
-    pid_t pid_1 = fork();
-
-    if (pid_1 < -1)
+    pid_t pid = fork();
+    if (pid < 0)
     {
         printf("Error while forking child process.\n");
         exit(1);
     }
-    else if (pid_1 == 0)
-    { // child process
+    else if (pid == 0)
+    {
         handle_incoming_calls();
         exit(0);
     }
 
-    // continue with parent process
-
+    // Fork counselor processes
     for (int i = 0; i < MAX_COUNSELORS; i++)
     {
-        pid_t pid_x = fork();
-
-        if (pid_x < -1)
+        pid = fork();
+        if (pid < 0)
         {
-            printf("Error while forking child process.\n");
+            printf("Error while forking counselor process.\n");
             exit(1);
         }
-        else if (pid_x == 0)
+        else if (pid == 0)
         {
             counselor();
             exit(0);
         }
     }
 
-    // Wait for all processes to finish
-    for (int i = 0; i < MAX_COUNSELORS; i++)
+    // Wait for all child processes to finish
+    for (int i = 0; i < MAX_COUNSELORS + 1; i++)
     {
         wait(NULL);
     }
 
-    // check for failure
-    // shmdt(shared_data);
-
-    // delete shared memory
-    // if (shmctl(shared_memory_id, IPC_RMID, NULL) == -1)
-    // {
-    //     printf("Error while deleting shared memory!\n");
-    //     exit(1);
-    // }
+    // Cleanup shared memory and semaphores
+    shmctl(shared_memory_id, IPC_RMID, NULL);
+    semctl(mutex_sem, 0, IPC_RMID);
+    semctl(waiting_sem, 0, IPC_RMID);
+    semctl(counselor_sem, 0, IPC_RMID);
 
     return 0;
 }
