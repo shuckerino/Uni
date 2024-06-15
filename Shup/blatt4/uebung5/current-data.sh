b68bc310f94c70c7b3456a380a645bc7f290a98f
Schuster, Rinaldo <rino.schu@gmail.com>
2024-06-15 21:06:06 +0200

    Shup update

--- Shup/blatt4/uebung5/current-data.sh
+++ Shup/blatt4/uebung5/current-data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 
-echo "Start von Shell-Skript $0 am `date`"
+echo "Start von Shell-Skript $0 am $(date)"
 echo "Die Prozess-ID des Shell-Skripts lautet: $$"
 echo "Shell-Skript wird aufgeführt von: $USER"
 echo "Das aktuelle Arbeitsverzeichnis ist: $PWD"
 echo "Das Home-Verzeichnis des aktuellen Benutzers ist: $HOME"
 echo "Die Pfadvariable hat den Wert: $PATH"
-echo "Der Rechner ist: ($HOSTNAME)"
+echo "Der Rechner ist: $(hostname)"
 echo "Das installierte System ist: `uname -a`"
 echo "Die verwendete Shell ist: $SHELL"
 echo "Das Terminal ist: $TERM"
