87cd4cf425e9a78d20b04c52d94ba142488319c9
Schuster, Rinaldo <rino.schu@gmail.com>
2024-11-06 14:45:45 +0100

    new find clumps still not working

--- Bioinformatics/Chapter03/Lab03/find_clumps.py
+++ Bioinformatics/Chapter03/Lab03/find_clumps.py
@@ -1,7 +1,7 @@
 import General.parse_input_files as parse_input_files
 from collections import defaultdict
 
-def find_clumps_faster(sequence : str, k : int, L: int, t) -> set:
+def find_clumps_faster(sequence : str, k : int, L: int, t : int) -> set[str]:
     '''Finding all kmer clumps in sequence\n
        Returns list of kmers, that are (L,t)-clumps in sequence'''
     kmer_to_index_dict = defaultdict(list) # mapping clump to list of indices
@@ -23,6 +23,49 @@ def find_clumps_faster(sequence : str, k : int, L: int, t) -> set:
             if index_list[i+t] - index_list[i] <= L: # if difference of indices is smaller than window size
                 found_clumps.add(kmer)
     return found_clumps
+
+def find_clumps_with_window(sequence : str, k : int, L : int, t : int) -> set[str]:
+    '''
+    We know want to only view the difference to the previous window
+    '''
+    found_clumps = set()
+    kmer_to_count_dict = dict()
+    
+    #get kmers of first_window
+    first_window = sequence[:L]
+    for i in range(len(first_window) - k + 1):
+        current_kmer = first_window[i:i+k]
+        if current_kmer in kmer_to_count_dict and current_kmer not in found_clumps: # ignore if already found
+            kmer_to_count_dict[current_kmer] += 1
+            if kmer_to_count_dict[current_kmer] >= t:
+                found_clumps.add(current_kmer)
+        else:
+            kmer_to_count_dict[current_kmer] = 1
+    
+    # print(f"kmer dict is {kmer_to_count_dict}")
+    
+    # iterare through the rest of windows
+    for i in range(1, len(sequence) - L):
+        window_L = sequence[i:i+L]
+        # print(f"window l is {window_L}")
+    
+        # update dict by removing element before
+        dropped_kmer = sequence[i-1:i-1+k]
+        # print(f"dropped kmer is {dropped_kmer}")
+        if dropped_kmer in kmer_to_count_dict:
+            kmer_to_count_dict[dropped_kmer] -= 1
+            if kmer_to_count_dict[dropped_kmer] == 0:
+                _ = kmer_to_count_dict.pop(dropped_kmer)
+        
+        # update dict by adding new element
+        new_kmer = window_L[L - k:]
+        if new_kmer in kmer_to_count_dict and new_kmer not in found_clumps: # ignore if already found
+            kmer_to_count_dict[new_kmer] += 1
+            if kmer_to_count_dict[new_kmer] >= t:
+                found_clumps.add(new_kmer)
+                
+    return found_clumps
+    
         
     
 if __name__ == "__main__":
@@ -31,5 +74,10 @@ if __name__ == "__main__":
     # sequence = "gatcagcataagggtccCTGCAATGCATGACAAGCCTGCAGTtgttttac".upper()
     # print(len(sequence))
     list_of_clumps = find_clumps_faster(sequence, k=9, L=540, t=6) # t = 6, L = len(oriC)
+    print(list_of_clumps)
+    
+    print(50 * "#")
+    
+    list_of_clumps = find_clumps_with_window(sequence, k=9, L=540, t=1) # t = 6, L = len(oriC)
     print(list_of_clumps)
         
