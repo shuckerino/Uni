19e465188b8be8a4a4bb4a19de48c01d8b719298
Schuster, Rinaldo <rino.schu@gmail.com>
2024-12-02 14:13:08 +0100

    Update in lecture

--- Bioinformatics/Chapter08/Lab08/money_change.py
+++ Bioinformatics/Chapter08/Lab08/money_change.py
@@ -18,8 +18,16 @@ def get_min_num_of_coins_greedy(total: int, coins: list[int]) -> list[int]:
 Task 1b: Is the greedy algo optimal for some coin list?
 
 Answer:
-NO, every coin list has the same flaw, because the paradigm of greedy is just not optimal. Greedy just follows the most promising solution, and because of that we do not find
-solutions that are "hidden"
+YES, because in this example, every predecessor coin is max the half of the next coin. So to replace a coin, you must use
+at least 2 smaller coins. So the biggest coin is always the right choice in order to use the minimum coin.
+
+'''
+
+
+'''
+Task 1d: How to optimize memory mangement for big total values?
+
+We only need to save |coins| values from before
 
 '''
 
@@ -33,10 +41,27 @@ def get_min_num_of_coins_dynamic_prog(total: int, coins: list[int]) -> list[int]
         subsolution_dict[m] = 999999999
         # for current value, check which coins fit in
         for i in range(len(coins)):
-            if m >= coins[i]:
+            if coins[i] <= m:
                 if subsolution_dict[m - coins[i]] + 1 < subsolution_dict[m]:
                     subsolution_dict[m] = subsolution_dict[m - coins[i]] + 1  # the current total - biggest coin that fit in + 1
     return subsolution_dict.get(total, -1)
+
+
+def get_min_num_of_coins_dynamic_prog_optimized(total: int, coins: list[int]) -> list[int]:
+    
+    max_coin_value = max(coins)
+    result_array = [None] * max_coin_value
+    result_array[0] = 0
+    current_value = 0  #counter to total
+    
+    while current_value <= total:
+        for m in range(1, len(result_array)):
+            result_array[m] = 999999999
+            for i in range(len(coins)):
+                if coins[i] <= m:
+                    if result_array[m - coins[i]] + 1 < result_array[m]:
+                        result_array[m] = result_array[m - coins[i]] + 1  # the current total - biggest coin that fit in + 1
+        return result_array.get(total, -1)
 
 
 if __name__ == "__main__":
